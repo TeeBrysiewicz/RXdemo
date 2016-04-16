e97fbf9da2908f6eb1d92ae979ac99357edc8ccf
Tobias Brysiewicz <tbrysiewicz@att.net>
2016-04-16 13:27:21 -0700

    RX demo built

---
+++ RXdemo/Pods/Target Support Files/Pods-RXdemo/Pods-RXdemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RXdemo : NSObject
+@end
+@implementation PodsDummy_Pods_RXdemo
+@end
---
+++ RXdemo/RXdemo/RXplayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+var str = "Hello, playground"
+
+
+print("Goodbye")
